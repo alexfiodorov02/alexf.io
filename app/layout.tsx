bcdfc2c1e12f886d113f1ad9342fc0dbf0503539
Alexander Fiodorov-Miller <alexfiodorov02@gmail.com>
2025-12-03 16:40:20 -0600

    Now using Fira Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,9 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0D1B2A" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         <Script
           id="structured-data"
           type="application/ld+json"
